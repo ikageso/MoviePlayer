a0d2285891ba63e2badf963cca37a581f32e0231
mon <monmon>
2015-08-18 15:13:23 +0900

    Change the acquisition process of the thumbnail

--- MoviePlayer/Model/MovieFileList.cs
+++ MoviePlayer/Model/MovieFileList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -59,7 +60,6 @@ namespace MoviePlayer.Model
                 GetFileInfo();
             }
         }
-        public string ThumbnaileFolderName { get; set; }
 
         /// <summary>
         /// 再生時間とタイトルのサムネイルを取得
@@ -68,57 +68,36 @@ namespace MoviePlayer.Model
         {
             Task.Run(async () =>
             {
+                #region アプリケーションアイコンを取得
                 foreach (var s in this)
                 {
                     try
                     {
-                        var me = new MediaPlayer()
-                        {
-                            ScrubbingEnabled = true,
-                            Volume = 0
-                        };
+                        IShellItem ppsi = null;
+                        IntPtr hbitmap = IntPtr.Zero;
+                        // GUID of IShellItem.
+                        Guid uuid = new Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe");
+                        SHCreateItemFromParsingName(s.FullPath, IntPtr.Zero, uuid, out ppsi);
+                        ((IShellItemImageFactory)ppsi).GetImage(new SIZE(256, 256), 0x0, out hbitmap);
 
-                        me.Open(new Uri(s.FullPath, UriKind.Absolute));
+                        BitmapSource bmpSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                            hbitmap,
+                            IntPtr.Zero,
+                            Int32Rect.Empty,
+                            System.Windows.Media.Imaging.BitmapSizeOptions.
+                            FromEmptyOptions()
+                            );
+                        
+                        Marshal.Release(hbitmap);
 
-                        // 読み込みが完了するまで待機
-                        while (me.DownloadProgress < 1.0 && me.NaturalVideoWidth == 0)
-                        {
-                            Thread.Sleep(100);
-                        }
-
-                        me.Pause();
-
-                        double pos = me.NaturalDuration.TimeSpan.TotalMilliseconds / 2;
-
-                        me.Position = TimeSpan.FromSeconds(pos);
-
-                        // リサイズ後のサイズを計算
-                        var ratio = (double)100 / me.NaturalVideoWidth;
-
-                        int width = (int)(me.NaturalVideoWidth * ratio);
-                        int height = (int)(me.NaturalVideoHeight * ratio);
-
-                        // 描画用のVisual
-                        var visual = new DrawingVisual();
-
-                        using (var context = visual.RenderOpen())
-                        {
-                            context.DrawVideo(me, new System.Windows.Rect(0, 0, width, height));
-                        }
-
-                        // レンダリングするビットマップ
-                        var bitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
-
-                        // レンダリング
-                        bitmap.Render(visual);
-
+                        JpegBitmapEncoder encoder = new JpegBitmapEncoder();
                         var bitmapImage = new BitmapImage();
-                        var bitmapEncoder = new PngBitmapEncoder();
-                        bitmapEncoder.Frames.Add(BitmapFrame.Create(bitmap));
+                        encoder.Frames.Add(BitmapFrame.Create(bmpSource));
+
 
                         using (var stream = new MemoryStream())
                         {
-                            bitmapEncoder.Save(stream);
+                            encoder.Save(stream);
                             stream.Seek(0, SeekOrigin.Begin);
 
                             bitmapImage.BeginInit();
@@ -134,7 +113,79 @@ namespace MoviePlayer.Model
                     {
                     }
                 }
+                #endregion
 
+
+                #region MediaPlayerでサムネイル作成
+                //foreach (var s in this)
+                //{                  
+                //    try
+                //    {
+                //        var me = new MediaPlayer()
+                //        {
+                //            ScrubbingEnabled = true,
+                //            Volume = 0
+                //        };
+
+                //        me.Open(new Uri(s.FullPath, UriKind.Absolute));
+
+                //        // 読み込みが完了するまで待機
+                //        while (me.DownloadProgress < 1.0 && me.NaturalVideoWidth == 0)
+                //        {
+                //            Thread.Sleep(100);
+                //        }
+
+                //        me.Pause();
+
+                //        double pos = me.NaturalDuration.TimeSpan.TotalMilliseconds / 2;
+
+                //        me.Position = TimeSpan.FromSeconds(pos);
+
+                //        // リサイズ後のサイズを計算
+                //        var ratio = (double)100 / me.NaturalVideoWidth;
+
+                //        int width = (int)(me.NaturalVideoWidth * ratio);
+                //        int height = (int)(me.NaturalVideoHeight * ratio);
+
+                //        // 描画用のVisual
+                //        var visual = new DrawingVisual();
+
+                //        using (var context = visual.RenderOpen())
+                //        {
+                //            context.DrawVideo(me, new System.Windows.Rect(0, 0, width, height));
+                //        }
+
+                //        // レンダリングするビットマップ
+                //        var bitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+
+                //        // レンダリング
+                //        bitmap.Render(visual);
+
+                //        var bitmapImage = new BitmapImage();
+                //        var bitmapEncoder = new PngBitmapEncoder();
+                //        bitmapEncoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+                //        using (var stream = new MemoryStream())
+                //        {
+                //            bitmapEncoder.Save(stream);
+                //            stream.Seek(0, SeekOrigin.Begin);
+
+                //            bitmapImage.BeginInit();
+                //            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                //            bitmapImage.StreamSource = stream;
+                //            bitmapImage.EndInit();
+                //            bitmapImage.Freeze();// BitmapImageの操作はUIで行われる。Freezeすることで、例外:「DependencySource は、DependencyObject と同じ Thread 上で作成する必要があります。」を回避する。
+                //        }
+
+                //        s.Image = bitmapImage;
+                //    }
+                //    catch
+                //    {
+                //    }
+                //}
+                #endregion
+
+                #region Shell32でファイル情報取得、ffmpegでサムネイル作成
                 ////
                 //// Shell32
                 ////
@@ -235,12 +286,92 @@ namespace MoviePlayer.Model
                 //        s.Image = bmpImage;
                 //    }
                 //    }
+                #endregion
 
                 await Task.Delay(30);
 
             });
 
         }
+
+        #region win32
+        public string ThumbnaileFolderName { get; set; }
+
+        [ComImportAttribute()]
+        [GuidAttribute("bcc18b79-ba16-442f-80c4-8a59c30c463b")]
+        [InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IShellItemImageFactory
+        {
+            void GetImage(
+            [In, MarshalAs(UnmanagedType.Struct)] SIZE size,
+            [In] SIIGBF flags,
+            [Out] out IntPtr phbm);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SIZE
+        {
+            public int cx;
+            public int cy;
+
+            public SIZE(int cx, int cy)
+            {
+                this.cx = cx;
+                this.cy = cy;
+            }
+        }
+
+        [Flags]
+        public enum SIIGBF
+        {
+            SIIGBF_RESIZETOFIT = 0x00,
+            SIIGBF_BIGGERSIZEOK = 0x01,
+            SIIGBF_MEMORYONLY = 0x02,
+            SIIGBF_ICONONLY = 0x04,
+            SIIGBF_THUMBNAILONLY = 0x08,
+            SIIGBF_INCACHEONLY = 0x10,
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
+        public interface IShellItem
+        {
+            void BindToHandler(IntPtr pbc,
+                [MarshalAs(UnmanagedType.LPStruct)]Guid bhid,
+                [MarshalAs(UnmanagedType.LPStruct)]Guid riid,
+                out IntPtr ppv);
+
+            void GetParent(out IShellItem ppsi);
+
+            void GetDisplayName(SIGDN sigdnName, out IntPtr ppszName);
+
+            void GetAttributes(uint sfgaoMask, out uint psfgaoAttribs);
+
+            void Compare(IShellItem psi, uint hint, out int piOrder);
+        };
+
+        public enum SIGDN : uint
+        {
+            NORMALDISPLAY = 0,
+            PARENTRELATIVEPARSING = 0x80018001,
+            PARENTRELATIVEFORADDRESSBAR = 0x8001c001,
+            DESKTOPABSOLUTEPARSING = 0x80028000,
+            PARENTRELATIVEEDITING = 0x80031001,
+            DESKTOPABSOLUTEEDITING = 0x8004c000,
+            FILESYSPATH = 0x80058000,
+            URL = 0x80068000
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        public static extern void SHCreateItemFromParsingName(
+        [In][MarshalAs(UnmanagedType.LPWStr)] string pszPath,
+        [In] IntPtr pbc,
+        [In][MarshalAs(UnmanagedType.LPStruct)] Guid riid,
+        [Out][MarshalAs(UnmanagedType.Interface, IidParameterIndex = 2)] out IShellItem ppv);
+
+
+        #endregion
     }
 
 }
